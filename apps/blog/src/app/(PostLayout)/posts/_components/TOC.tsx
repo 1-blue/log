8ec2f0f8fd94646eec3d159a3b2ea62786dd1b18
상은 <1-blue98@naver.com>
2025-09-26 14:43:12 +0900

    🚸: TOC 위치 변경

--- apps/blog/src/app/(PostLayout)/posts/_components/TOC.tsx
+++ apps/blog/src/app/(PostLayout)/posts/_components/TOC.tsx
@@ -127,7 +127,7 @@ const TOC: React.FC<IProps> = ({ toc }) => {
   }
 
   return (
-    <nav className="bg-accent fixed top-20 right-4 z-50 w-64 rounded-lg text-xs shadow-md">
+    <nav className="bg-accent fixed top-4 right-4 z-50 w-64 rounded-lg text-xs shadow-md">
       <Button
         onClick={() => setIsExpanded(false)}
         variant="ghost"
