0b1fee8b09b4020343138458e6581fe9b8671ea4
1-blue <1-blue98@naver.com>
2026-03-15 17:01:08 +0900

    🐛: 타입 단언으로 인한 제목 이슈 처리

--- apps/blog/src/components/mdx/Heading.tsx
+++ apps/blog/src/components/mdx/Heading.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const anchorClassName =
   "hover:after:visible after:invisible lg:after:absolute after:top-0 lg:after:right-full after:ml-2 lg:after:mr-2 after:px-2 after:bg-accent after:text-primary after:rounded-md after:content-['#']";
 
-interface Props extends React.HTMLAttributes<HTMLHeadingElement> {}
+interface Props extends React.HTMLAttributes<HTMLHeadingElement> { }
 
 const H1: React.FC<React.PropsWithChildren<Props>> = ({
   children,
@@ -15,7 +15,7 @@ const H1: React.FC<React.PropsWithChildren<Props>> = ({
     {...restProps}
   >
     <Link
-      href={"#" + (children as string).replaceAll(" ", "-").toLowerCase()}
+      href={"#" + (children?.toString() ?? "").replaceAll(" ", "-").toLowerCase()}
       className={anchorClassName}
     >
       {children}
@@ -32,7 +32,7 @@ const H2: React.FC<React.PropsWithChildren<Props>> = ({
     {...restProps}
   >
     <Link
-      href={"#" + (children as string).replaceAll(" ", "-").toLowerCase()}
+      href={"#" + (children?.toString() ?? "").replaceAll(" ", "-").toLowerCase()}
       className={anchorClassName}
     >
       {children}
@@ -49,7 +49,7 @@ const H3: React.FC<React.PropsWithChildren<Props>> = ({
     {...restProps}
   >
     <Link
-      href={"#" + (children as string).replaceAll(" ", "-").toLowerCase()}
+      href={"#" + (children?.toString() ?? "").replaceAll(" ", "-").toLowerCase()}
       className={anchorClassName}
     >
       {children}
@@ -66,7 +66,7 @@ const H4: React.FC<React.PropsWithChildren<Props>> = ({
     {...restProps}
   >
     <Link
-      href={"#" + (children as string).replaceAll(" ", "-").toLowerCase()}
+      href={"#" + (children?.toString() ?? "").replaceAll(" ", "-").toLowerCase()}
       className={anchorClassName}
     >
       {children}
@@ -83,7 +83,7 @@ const H5: React.FC<React.PropsWithChildren<Props>> = ({
     {...restProps}
   >
     <Link
-      href={"#" + (children as string).replaceAll(" ", "-").toLowerCase()}
+      href={"#" + (children?.toString() ?? "").replaceAll(" ", "-").toLowerCase()}
       className={anchorClassName}
     >
       {children}
@@ -100,7 +100,7 @@ const H6: React.FC<React.PropsWithChildren<Props>> = ({
     {...restProps}
   >
     <Link
-      href={"#" + (children as string).replaceAll(" ", "-").toLowerCase()}
+      href={"#" + (children?.toString() ?? "").replaceAll(" ", "-").toLowerCase()}
       className={anchorClassName}
     >
       {children}
